320546f5ef20d9695c159fa11dde80127cdb450c
Kevizy <44855147+Kevizy@users.noreply.github.com>
2020-07-21 21:53:35 -0700

    Add files via upload

---
+++ Text_Editor/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Text_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFormat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBold = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItalic = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuIUnderline = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSizeFormat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFormatSmallSize = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFormatMediumSize = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFormatLargeSize = new System.Windows.Forms.ToolStripMenuItem();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Location = new System.Drawing.Point(12, 40);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEditor.Size = new System.Drawing.Size(979, 581);
+            this.txtEditor.TabIndex = 0;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuFormat});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(994, 24);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.openToolStripMenuItem,
+            this.mnuSave,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.Size = new System.Drawing.Size(180, 22);
+            this.mnuNew.Text = "&New";
+            this.mnuNew.Click += new System.EventHandler(this.mnuNew_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(180, 22);
+            this.mnuSave.Text = "&Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(180, 22);
+            this.mnuExit.Text = "&Exit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // mnuFormat
+            // 
+            this.mnuFormat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuBold,
+            this.mnuItalic,
+            this.mnuIUnderline,
+            this.mnuSizeFormat});
+            this.mnuFormat.Name = "mnuFormat";
+            this.mnuFormat.Size = new System.Drawing.Size(57, 20);
+            this.mnuFormat.Text = "F&ormat";
+            // 
+            // mnuBold
+            // 
+            this.mnuBold.Name = "mnuBold";
+            this.mnuBold.Size = new System.Drawing.Size(180, 22);
+            this.mnuBold.Text = "&Bold";
+            this.mnuBold.Click += new System.EventHandler(this.mnuBold_Click);
+            // 
+            // mnuItalic
+            // 
+            this.mnuItalic.Name = "mnuItalic";
+            this.mnuItalic.Size = new System.Drawing.Size(180, 22);
+            this.mnuItalic.Text = "&Italic";
+            this.mnuItalic.Click += new System.EventHandler(this.mnuItalic_Click);
+            // 
+            // mnuIUnderline
+            // 
+            this.mnuIUnderline.Name = "mnuIUnderline";
+            this.mnuIUnderline.Size = new System.Drawing.Size(180, 22);
+            this.mnuIUnderline.Text = "&Underline";
+            this.mnuIUnderline.Click += new System.EventHandler(this.mnuIUnderline_Click);
+            // 
+            // mnuSizeFormat
+            // 
+            this.mnuSizeFormat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFormatSmallSize,
+            this.mnuFormatMediumSize,
+            this.mnuFormatLargeSize});
+            this.mnuSizeFormat.Name = "mnuSizeFormat";
+            this.mnuSizeFormat.Size = new System.Drawing.Size(180, 22);
+            this.mnuSizeFormat.Text = "Si&ze";
+            // 
+            // mnuFormatSmallSize
+            // 
+            this.mnuFormatSmallSize.Checked = true;
+            this.mnuFormatSmallSize.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mnuFormatSmallSize.Name = "mnuFormatSmallSize";
+            this.mnuFormatSmallSize.Size = new System.Drawing.Size(180, 22);
+            this.mnuFormatSmallSize.Text = "&Small";
+            this.mnuFormatSmallSize.Click += new System.EventHandler(this.mnuSizeFormat_Click);
+            // 
+            // mnuFormatMediumSize
+            // 
+            this.mnuFormatMediumSize.Name = "mnuFormatMediumSize";
+            this.mnuFormatMediumSize.Size = new System.Drawing.Size(180, 22);
+            this.mnuFormatMediumSize.Text = "&Medium";
+            // 
+            // mnuFormatLargeSize
+            // 
+            this.mnuFormatLargeSize.Name = "mnuFormatLargeSize";
+            this.mnuFormatLargeSize.Size = new System.Drawing.Size(180, 22);
+            this.mnuFormatLargeSize.Text = "&Large";
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "openFileDialog1";
+            this.dlgOpen.Filter = "Text Files (*.txt)|*.txt";
+            this.dlgOpen.Title = "Open File";
+            // 
+            // dlgSave
+            // 
+            this.dlgSave.Filter = "Text Files (*.txt)|*.txt";
+            this.dlgSave.Title = "Save File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 633);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuFormat;
+        private System.Windows.Forms.ToolStripMenuItem mnuBold;
+        private System.Windows.Forms.ToolStripMenuItem mnuItalic;
+        private System.Windows.Forms.ToolStripMenuItem mnuIUnderline;
+        private System.Windows.Forms.ToolStripMenuItem mnuSizeFormat;
+        private System.Windows.Forms.ToolStripMenuItem mnuFormatSmallSize;
+        private System.Windows.Forms.ToolStripMenuItem mnuFormatMediumSize;
+        private System.Windows.Forms.ToolStripMenuItem mnuFormatLargeSize;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+    }
+}
+
---
+++ Text_Editor/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace Text_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sets default font size and font style
+        int fontsize = 8;
+        string fontName = "MS Sans Serif"; 
+
+        // method to change the font 
+        public void ChangeFont()
+        {
+            FontStyle newFont;
+            newFont = FontStyle.Regular; 
+
+            // Checks if font buttons are clicked 
+            if (mnuBold.Checked)
+            {
+                newFont = newFont | FontStyle.Bold; 
+            }
+
+            if (mnuItalic.Checked)
+            {
+                newFont = newFont | FontStyle.Italic; 
+            }
+
+            if (mnuIUnderline.Checked)
+            {
+                newFont = newFont | FontStyle.Underline; 
+            }
+
+            // Changes the font in text editor 
+            txtEditor.Font = new Font(fontName, fontsize, newFont); 
+        }
+
+        // Method to create a new file 
+        private void mnuNew_Click(object sender, EventArgs e)
+        {
+            // Ask the user if wants to start a new file 
+            if (MessageBox.Show("Are you sure you want to start a new file?", "New File", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                txtEditor.Text = ""; 
+            }
+        }
+
+        // Method to close the program 
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            // Checks if user wants to close program
+            if (MessageBox.Show("Are you sure you want to exit the program??", "Exit Application", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+               Close();
+            }
+        }
+
+
+
+        // Method to save the file 
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            if (dlgSave.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter outputFile = new StreamWriter(dlgSave.FileName);
+                outputFile.Write(txtEditor.Text);
+                outputFile.Close();
+            } 
+        }
+
+        private void mnuBold_Click(object sender, EventArgs e)
+        {
+            // Sets the property to checked if its not checked 
+            mnuBold.Checked = !mnuBold.Checked;
+            ChangeFont(); 
+        }
+
+        private void mnuItalic_Click(object sender, EventArgs e)
+        {
+            mnuItalic.Checked = !mnuItalic.Checked;
+            ChangeFont(); 
+        }
+
+        private void mnuIUnderline_Click(object sender, EventArgs e)
+        {
+            mnuIUnderline.Checked = !mnuIUnderline.Checked;
+            ChangeFont();
+        }
+
+        private void mnuSizeFormat_Click(object sender, EventArgs e)
+        {
+            string sizeClicked = ((ToolStripMenuItem)sender).Text;
+            mnuFormatSmallSize.Checked = false;
+            mnuFormatMediumSize.Checked = false;
+            mnuFormatLargeSize.Checked = false; 
+
+            switch (sizeClicked)
+            {
+                case "&Small":
+                    fontsize = 8;
+                    mnuFormatSmallSize.Checked = true;
+                    break;
+                case "&Medium":
+                    fontsize = 11;
+                    mnuFormatMediumSize.Checked = true;
+                    break;
+                case "&Large":
+                    fontsize = 16;
+                    mnuFormatLargeSize.Checked = true;
+                    break; 
+            }
+            ChangeFont(); 
+        }
+
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            // Check if file name is open 
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                // creates a new streamreader and opens text 
+                StreamReader inputFile = new StreamReader(dlgOpen.FileName);
+                txtEditor.Text = inputFile.ReadToEnd();
+                inputFile.Close();
+                txtEditor.SelectionLength = 0; 
+            }
+        }
+    }
+}
